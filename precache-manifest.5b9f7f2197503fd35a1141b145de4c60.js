5e319fa03072c7ea09473f789cedcc2bc4117c07
Bartosy <drave12345@gmail.com>
2020-10-20 14:47:25 +0200

    Updates

---
+++ precache-manifest.5b9f7f2197503fd35a1141b145de4c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e3941ed035ed538147c4b9f699a9c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "901f17813a307a091fbc",
+    "url": "/monsters-rolodex/static/css/main.f046548d.chunk.css"
+  },
+  {
+    "revision": "56aae774d8f9eeb2cd6c",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901f17813a307a091fbc",
+    "url": "/monsters-rolodex/static/js/main.c733edf6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
